29ccdc990c78731c1ceeef13bb85ad546b4ec4db
AlfinH <47842410+AlfinH@users.noreply.github.com>
2019-12-29 18:56:36 +0700

    Attribute Variable
    Range

--- Assets/Latihan2.cs
+++ Assets/Latihan2.cs
@@ -6,11 +6,13 @@ public class Latihan2 : MonoBehaviour
 {
     public int nilaiA;
     public int nilaiB;
+    [Range(1, 5)]
+    public int nilaiC;
 
     // Use this for initialization
     void Start()
     {
-        int totall = nilaiA + nilaiB;
+        int totall = (nilaiA + nilaiB) * nilaiC;
         Debug.Log("Total " + totall);
     }
 
